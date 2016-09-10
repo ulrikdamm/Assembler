1113176c4bcc5d499742c0c08817fe49683c1d9d
Ulrik Damm <ulrikdamm@me.com>
2016-09-10 14:48:05 +0200

    Initial Commit

---
+++ Assembler/Assembler.h
@@ -0,0 +1,19 @@
+//
+//  Assembler.h
+//  Assembler
+//
+//  Created by Ulrik Damm on 10/09/2016.
+//  Copyright © 2016 Ufd.dk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Assembler.
+FOUNDATION_EXPORT double AssemblerVersionNumber;
+
+//! Project version string for Assembler.
+FOUNDATION_EXPORT const unsigned char AssemblerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Assembler/PublicHeader.h>
+
+
