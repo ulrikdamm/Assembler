8d68364b1518245ecd15d75c3238157b0a3fa3d3
Ulrik Damm <ulrikdamm@me.com>
2018-11-08 10:27:16 +0100

    Fixed Swift Package Manager manifest file

--- Package.swift
+++ Package.swift
@@ -1,3 +1,27 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let package = Package(name: "Assembler")
+let package = Package(
+    name: "Assembler",
+    products: [
+        .executable(name: "Assembler", targets: ["Assembler"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Assembler",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "AssemblerTests",
+            dependencies: ["Assembler"],
+            path: "Tests"),
+    ]
+)
